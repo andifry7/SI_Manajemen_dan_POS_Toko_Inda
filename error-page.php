a1e95b677d98d3b66ba2f9ff32c59f9201a0e67a
Andifry <andifry.github7@gmail.com>
2026-06-25 00:16:20 +0700

    fix: memperbaiki footer dan isi paragraf

--- error-page.php
+++ error-page.php
@@ -46,7 +46,7 @@ require "template/sidebar.php";
 
           <p>
             We could not find the page you were looking for.
-            Meanwhile, you may <a href="dashboard.php">return to dashboard</a> or try using the search form.
+            Meanwhile, you may <a href="dashboard.php">return to dashboard</a>
           </p>
 
         </div>
@@ -55,30 +55,8 @@ require "template/sidebar.php";
       <!-- /.error-page -->
     </section>
     <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0
-    </div>
-    <strong>Copyright &copy; 2026 <a href="">Toko Inda</a>.</strong> All rights reserved.
-  </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
-</body>
-</html>
+    <?php 
+    
+    require "template/footer.php";
+    
+    ?>
