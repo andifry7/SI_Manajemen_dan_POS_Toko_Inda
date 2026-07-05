370cc72f75043699f69db09aa4467d092bf1c833
Andifry <andifry.github7@gmail.com>
2026-07-06 01:44:46 +0700

    feat: membuat fitur halaman laporan penjualan barang dengan filter tanggal

---
+++ laporan-penjualan/detail-penjualan.php
@@ -0,0 +1,92 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['ssLoginPOS'])) {
+    header("location: ../auth/login.php");
+    exit();
+}
+
+require "../config/config.php";
+require "../config/functions.php";
+require "../module/mode-barang.php";
+
+$title = "Laporan Penjualan - Toko Inda POS";
+require "../template/header.php";
+require "../template/navbar.php";
+require "../template/sidebar.php";
+
+$id = $_GET['id'];
+$tgl = $_GET['tgl'];
+$penjualan = getData("SELECT * FROM tbl_jual_detail WHERE no_jual = '$id'");
+
+?>
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Detail Penjualan</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="<?= $main_url ?>dashboard.php">Home</a></li>
+              <li class="breadcrumb-item"><a href="<?= $main_url ?>laporan-penjualan/index.php">Laporan Penjualan</a></li>
+              <li class="breadcrumb-item active">Detail Penjualan</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title"><i class="fas fa-list fa-sm"></i> Rincian Barang</h3>
+                    <button type="button" class="btn btn-sm btn-success float-right"><?= $tgl ?></button>
+                    <button type="button" class="btn btn-sm btn-warning float-right mr-1"><?= $id ?></button>
+                </div>
+                <div class="card-body table-responsive p-3">
+                    <table class="table table-hover text-nowrap">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Kode Barang</th>
+                                <th>Nama Barang</th>
+                                <th>Kategori</th>
+                                <th>Harga Beli</th>
+                                <th class="text-center">Jumlah</th>
+                                <th class="text-center">Total Harga</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $no = 1;
+                            foreach ($penjualan as $jual) { ?>
+                                <tr>
+                                    <td><?= $no++ ?></td>
+                                    <td><?= $jual['barcode'] ?></td>
+                                    <td><?= $jual['nama_brg'] ?></td>
+                                    <td><?= $jual['kategori'] ?></td>
+                                    <td class="text-right"><?= number_format($jual['harga_jual'], 0, ',', '.') ?></td>
+                                    <td class="text-center"><?= $jual['qty'] ?></td>
+                                    <td class="text-right"><?= number_format($jual['jml_harga'], 0, ',', '.') ?></td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php
+
+require "../template/footer.php";
+
+?>
--- template/sidebar.php
+++ template/sidebar.php
@@ -92,7 +92,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="<?= $main_url ?>laporan-penjualan" class="nav-link">
                     <i class="nav-icon fas fa-chart-line text-sm"></i>
                     <p>
                         Laporan Penjualan
