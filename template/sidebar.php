1f9a563323dbb5fe8779f56dd8c5f6fea5f5d696
Andifry <andifry.github7@gmail.com>
2026-07-07 00:25:19 +0700

    style: menambahkan nama lengkap di sidebar samping foto user

--- template/sidebar.php
+++ template/sidebar.php
@@ -14,7 +14,7 @@
           <img src="<?= $main_url ?>asset/image/<?= userLogin()['foto'] ?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"><?= 'Ahmad Andi Alfiansyah' ?></a>
+          <a href="#" class="d-block\"><?= userLogin()['fullname'] ?></a>
         </div>
       </div>
 
