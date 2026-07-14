f53e608a3902e9616b99410d06f2d9247b88be1b
Andifry <andifry.github7@gmail.com>
2026-07-14 23:01:16 +0700

    style: mengganti icon plus menjadi pen di halaman edit satuan

--- satuan/edit-satuan.php
+++ satuan/edit-satuan.php
@@ -59,7 +59,7 @@ $satuan = getData($sqlEdit)[0];
         <div class="card">
             <form action="" method="post">
                 <div class="card-header">
-                    <h3 class="card-title"><i class="fas fa-plus fa-sm"></i> Edit Satuan</h3>
+                    <h3 class="card-title"><i class="fas fa-pen fa-sm"></i> Edit Satuan</h3>
                     <button type="submit" name="update" class="btn btn-primary btn-sm float-right"><i class="fas fa-save"></i> Update</button>
                     <button type="reset" class="btn btn-danger btn-sm float-right mr-1"><i class="fa fa-undo fa-sm"></i> Reset</button>
                 </div>
